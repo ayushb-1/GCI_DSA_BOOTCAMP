db3e259a1ad01354e6f09d264cc13e80c17e45e1
ayush bhatia <101511675+ayushb-1@users.noreply.github.com>
2022-03-23 03:59:13 -0700

    Add files via upload

---
+++ Milestone 14/Subset Sums.cpp
@@ -0,0 +1,23 @@
+// Subset sums(GFG Problem)
+class Solution
+{
+    public:
+    void func(int ind, int sum, vector<int> &arr, int N, vector<int> sumSubset){
+        if(ind==N){
+            sumSubset.push_back(sum);
+            return;
+        }
+        // when element picked
+        func(ind+1, sum+arr[ind], arr, N, sumSubset);
+        //when element not picked 
+        func(ind+1, sum, arr, N, sumSubset);
+    }
+public:
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+        vector<int> sumSubset;
+        func(0, 0, arr, N, sumSubset);
+        sort(sumSubset.begin(),sumSubset.end());
+        return sumSubset;
+    }
+};
