253d10e263625c80720c5d02bca0e6cec046887f
ayush bhatia <101511675+ayushb-1@users.noreply.github.com>
2022-03-24 09:00:08 -0700

    Add files via upload

---
+++ Milestone 14/shuffle integers.cpp
@@ -0,0 +1,11 @@
+void shuffleArray(int arr[],int n)
+	{
+	    int temp[n];
+	    int index=0;
+	    for(int i=0;i<n/2;i++){
+	    temp[index++]=arr[i];
+	    temp[index++]=arr[i+n/2];
+	}
+	for (int i=0;i<n;i++)
+	arr[i]=temp[i];
+	}
