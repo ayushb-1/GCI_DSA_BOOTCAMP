5032d00cb80f7652318c02ee4caa67f14bf0137a
ayush bhatia <101511675+ayushb-1@users.noreply.github.com>
2022-03-28 09:47:51 -0700

    uploaded permutation with case changes

---
+++ Milestone 14/Permutation with Case changes.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+void solve(string ip,string op)
+{
+    if(ip.length()==0)
+    {
+        cout<<op<<endl;
+    }
+
+    string op1=op;
+    string op2=op;
+
+    op1.push_back(ip[0]);
+    op2.push_back(toupper(ip[0]));
+    ip.erase(ip.begin()+0);
+
+    solve(ip,op1);
+    solve(ip,op2);
+    return;
+}
